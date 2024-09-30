4673862c512d06abaaef5ac0793528731c1b6110
Sahil Adhawade <sahil.adhawade@gmail.com>
2024-09-30 16:09:39 -0700

    progress on doc parser script; working on agent workflow for answering questions

--- doc_agent/doc_parser.py
+++ doc_agent/doc_parser.py
@@ -1,33 +1,76 @@
+import os
 import torch
 import numpy as np 
+from prompt_templates import (
+    QUERY_PROMPT, 
+    CONTEXT_TEMPLATE, 
+    SEMANTIC_CONTEXT_TEMPLATE
+)
 from model import Model
-from transformers import GPT2Tokenizer
+from dataclasses import dataclass
 from typing import Optional
+from typing import List
 from typing import Any
 
+@dataclass
+class Document: 
+    content: str 
+    semantic_context: str 
 
-EMBEDDING_MODEL = ""
+@dataclass 
+class Citation: 
+    document: Document 
+    similarity_score: float
+
+
+EMBEDDING_MODEL = "text-embedding-ada-002"
 
 
 class Preprocessor: 
+    llm = Model(model_name="gpt-4o")
 
     @staticmethod 
-    def pipeline(): 
-        pass 
+    def _get_semantic_context(content_chunk: str, full_document: str) -> str: 
+        query = SEMANTIC_CONTEXT_TEMPLATE.format(content=content_chunk, full_document=full_document)
+        response = Preprocessor.llm(query) 
+        return response
 
+    @staticmethod 
+    def _handle_csv(contents: str):
+        docs = []
+        rows = contents.split("\n")
+        col_names = rows[0].split(",")
+        for r in rows[1:]: 
+            vals = r.split(",")
+            content = ", ".join(f"{col_names[i]}: {vals[i]}" for i in range(len(col_names)))
+            semantic_context = Preprocessor._get_semantic_context(content, contents)
+            docs.append(Document(content, semantic_context))
+        return docs 
 
-class Parser(Model): 
-    def __init__(self, similarity_metric: Optional[str] = "cosine", **kwargs: Any):
-        super().__init__(**kwargs)
+    @staticmethod 
+    def _handle_txt(contents: str): 
+        pass
+
+    @staticmethod 
+    def preprocess_text(contents: str, file_type: str) -> List[Document]: 
+        if file_type == "csv": 
+            return Preprocessor._handle_csv(contents)
+        elif file_type == "txt": 
+            return Preprocessor._handle_txt(contents)  
+        else: 
+            raise ValueError(f"'{file_type}' is not currently supported.")
+
+class Parser: 
+    def __init__(self, model: Model, similarity_metric: Optional[str] = "cosine"):
+        self.llm = model.llm
 
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
         ) 
-        self.docs = [] 
+        self.documents = [] 
         self.vector_store = np.array([])
-        self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
         if hasattr(Parser, f"{similarity_metric}_similarity"): 
-            self.similarity = getattr(Parser, f"{similarity_metric}_similarity")
+            self.similarity_fn = getattr(Parser, f"{similarity_metric}_similarity")
         else: 
             raise ValueError(f"{similarity_metric} is NOT supported. Please choose 'cosine' or 'euclidean'.")
 
@@ -42,12 +85,61 @@ class Parser(Model):
         dists = np.linalg.norm(vector_embd - query_embd, axis=1)
         return 1 / (1 + dists)
 
-    def add_file(self, file_name: str): 
-        pass 
-
     def get_embedding_single(self, text_chunk: str) -> np.ndarray: 
         response = self.llm.embeddings.create(input=[text_chunk], model=EMBEDDING_MODEL)
-        return response.data[0].embedding 
+        return np.array(response.data[0].embedding) 
     
     def get_embedding_all(self, documents) -> np.ndarray:
         return np.array([self.get_embedding_single(doc) for doc in documents]) 
+
+    def add_file(self, path: str): 
+        assert os.path.isfile(path), f"The path provided ({path}) could not be found."
+
+        contents = open(path, "r").read() 
+        preprocessed_docs = Preprocessor.preprocess_text(contents, path.split(".")[-1])
+      
+        self.documents.extend([doc for doc in preprocessed_docs])
+        embeddings = np.vstack([
+            self.get_embedding_single(doc.content) for doc in preprocessed_docs
+        ])
+        if self.vector_store.size == 0: 
+            self.vector_store = embeddings 
+        else: 
+            self.vector_store = np.vstack((self.vector_store, embeddings))
+        
+    def search(self, query: str, top_k: int = 5) -> List[Citation]: 
+        query_embd = self.get_embedding_single(query)
+        similarity_scores = self.similarity_fn(self.vector_store, query_embd)
+        top_inds = np.argsort(similarity_scores)[::-1]
+
+        citations = []
+        for ind in top_inds:
+            citations.append(Citation(self.documents[ind], similarity_scores[ind]))
+            if len(citations) >= top_k: 
+                break 
+        return citations
+
+    def ground_query(self, query: str, citations: List[Citation]) -> str:
+        context = "" 
+        for i, citation in enumerate(citations): 
+            context += CONTEXT_TEMPLATE.format(
+                index=i+1, content=citation.document.content, semantic_context=citation.document.semantic_context
+            )
+        return QUERY_PROMPT.format(context=context, prompt=query)
+
+
+if __name__ == "__main__":
+    m = Model(model_name="gpt-4o") 
+    parser = Parser(m)
+    
+    parser.add_file("/Users/sahil/Documents/doc_agent/doc_agent/data/Grades.csv")
+
+    print(len(parser.documents))
+    print(parser.vector_store.size)
+
+    prompt = "What did student 1 get in Math?"
+    citations = parser.search(prompt)
+    new_prompt = parser.ground_query(prompt, citations)
+    print(new_prompt)
+
+    print(m(new_prompt))
--- doc_agent/prompt_templates.py
+++ doc_agent/prompt_templates.py
@@ -1,7 +1,30 @@
-SYSTEM_PROMPT = r"""
-    You are a helpful assistant chat bot that answers user queries accurately.
-    You must ground your answers in evidence that you have found from your
-    search, and you must address all parts of the question asked. You may ask 
-    clarifying questions if the question is unclear. DO NOT hallucinate. 
+SYSTEM_PROMPT = f"""
+You are a helpful assistant chat bot that answers user queries accurately.
+You must ground your answers in evidence that you have found from your
+search, and you must address all parts of the question asked. DO NOT hallucinate. 
+"""
+
+QUERY_PROMPT = """
+Answer the question based on on the information provided in the context below. 
+If the answer cannot be determined from the context, say "I do not have enough information to answer that question." 
+Do not use any external knowledge. Be concise and to the point. 
+
+Context: 
+{context}
+
+Question: {prompt}
+
+Answer: 
+"""
+
+CONTEXT_TEMPLATE = """#{index}\nRaw Content: {content}\nSemantic Context: {semantic_context}\n\n"""
+
+SEMANTIC_CONTEXT_TEMPLATE = """
+Generate a one to two sentence summary of the raw content within the context of the full document. 
+Do not use any external knowledge. Be concise and to the point. 
+
+Raw Content: {content}
+
+Full Document: {full_document}
 """
 
